adf99ce6808fe3991cbdae52c7a95fee613e19e5
Luana Kimley <luanakimley@gmail.com>
2022-11-03 21:22:49 +0000

    Set up Flask application with simple login with session.

---
+++ webserver/application.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+
+@app.route("/")
+def index():
+    if not session.get("name"):
+        return redirect("/login")
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        name = request.form.get("name")
+        session["name"] = name
+        return redirect("/")
+    #     Remember that user is logged in
+    #     Redirect to /
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run()
