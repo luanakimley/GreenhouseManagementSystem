81151b6c7c39bc0cbf3a24b697adf4db81b6230d
Luana Kimley <luanakimley@gmail.com>
2022-11-12 00:11:49 +0000

    :sparkles: Link MySQL database and register functionality done

--- webserver/application.py
+++ webserver/application.py
@@ -1,11 +1,27 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+from flask_mysqldb import MySQL
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = 'GMS'
+
+print(os.getenv('MYSQL_USER'))
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
+
+mysql = MySQL(app)
 
 
 @app.route("/")
@@ -15,6 +31,7 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # TODO: fix login functionality
     if request.method == "POST":
         name = request.form.get("name")
         session["name"] = name
@@ -25,6 +42,22 @@ def login():
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # TODO: encrypt password
+    # TODO: ensure no duplicate email and username
+    cursor = mysql.connection.cursor()
+    cursor.execute('''insert into Users(username, email, password) values (%s, %s, %s)''', (username, email, password))
+    mysql.connection.commit()
+    cursor.close()
+
+    return redirect("/landing")
 
 
 @app.route("/logout")
